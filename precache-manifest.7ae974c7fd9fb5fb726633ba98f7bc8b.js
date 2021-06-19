0727ff0a7fbfb56924a6d12637ee3e46ae0b4be5
sw1227 <sw1227@users.noreply.github.com>
2021-06-19 06:38:41 +0000

    deploy: 8c4b0d9621e5d0cfe6ea5c5a75b997784e9230d1

---
+++ precache-manifest.7ae974c7fd9fb5fb726633ba98f7bc8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6215ba08c479a62363f4",
+    "url": "/radar-map/css/app.914eb774.css"
+  },
+  {
+    "revision": "6215ba08c479a62363f4",
+    "url": "/radar-map/js/app.db82da33.js"
+  },
+  {
+    "revision": "982e6ef0ea8c79e3b5b4",
+    "url": "/radar-map/js/chunk-vendors.de0b5f0d.js"
+  },
+  {
+    "revision": "db1a8696a212d3f4a1c4a8688981746e",
+    "url": "/radar-map/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/radar-map/robots.txt"
+  }
+];
